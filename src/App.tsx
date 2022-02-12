149358b1ae99b64de03e5df63093e79d71c011a9
용석 박 <4up456@naver.com>
2022-02-12 17:14:08 +0900

    아무데나 클릭해도 색상 전체가 바뀜

--- src/App.tsx
+++ src/App.tsx
@@ -7,45 +7,19 @@ function App() {
   const [stage, setStage] = useState(1);
   const [time, setTime] = useState(15);
   const [score, setScore] = useState(0);
-  const [block, setBlock] = useState(4);
-  const [style, setStyle] = useState({
-    backgroundColor: 'aquamarine',
-    width: '175px',
-    height: '175px',
-  });
 
   const handleClickSquare = (event: any) => {
     if (event.target.dataset.id === 'equivalent') {
       setTime((prevTime) => prevTime - 3);
     } else {
       setStage((prevStage) => prevStage + 1);
-      if (stage > 1) {
-        const newBlock = Math.pow(Math.floor(stage + 1 / 2 + 1), 2);
-        setBlock((prevBlock) => (prevBlock = newBlock));
-      }
     }
   };
-
-  useEffect(() => {
-    if (stage > 1 && stage < 14) {
-      const newStyle = {
-        backgroundColor: 'rgb(0, 0 ,0)',
-        width: `${Math.floor(340 / Math.floor(stage + 1 / 2))}px`,
-        height: `${Math.floor(340 / Math.floor(stage + 1 / 2))}px`,
-      };
-      setStyle((prevStyle) => (prevStyle = newStyle));
-    }
-  }, [stage]);
 
   return (
     <>
       <GameProgressInformation stage={stage} time={time} score={score} />
-      <GameZone
-        style={style}
-        stage={stage}
-        block={block}
-        onClick={handleClickSquare}
-      />
+      <GameZone stage={stage} onClick={handleClickSquare} />
     </>
   );
 }
--- src/components/gameZone.tsx
+++ src/components/gameZone.tsx
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../css/gameZone.css';
 
-const GameZone = (props: {
-  style: { backgroundColor: string; width: string; height: string };
-  block: number;
-  stage: number;
-  onClick: (event: any) => void;
-}) => {
-  const randomIndex = Math.floor(Math.random() * props.block);
+const GameZone = (props: { stage: number; onClick: (event: any) => void }) => {
+  const block = Math.pow(Math.floor(props.stage / 2) + 1, 2);
+
+  const randomIndex = Math.floor(Math.random() * block);
+  console.log(`stage: ${props.stage}, block: ${block}`);
+
+  const randomRgb1 = Math.floor(Math.random() * 230);
+  const randomRgb2 = Math.floor(Math.random() * 230);
+  const randomRgb3 = Math.floor(Math.random() * 230);
+  const diffrentColorGap = 60 - props.stage * 2;
+  const newStyle = {
+    backgroundColor: `rgb(${randomRgb1}, ${randomRgb2} ,${randomRgb3})`,
+    width: `${Math.floor(340 / Math.pow(block, 0.5)) + 0.7}px`,
+    height: `${Math.floor(340 / Math.pow(block, 0.5)) + 0.7}px`,
+  };
+  const diffrentStyle = {
+    ...newStyle,
+    backgroundColor: `rgb(
+      ${randomRgb1 - diffrentColorGap}, 
+      ${randomRgb2 - diffrentColorGap} ,
+      ${randomRgb3 - diffrentColorGap})`,
+  };
 
   return (
     <div className="game-board">
-      {[...Array(props.block)].map((_, index) => {
+      {[...Array(block)].map((_, index) => {
         if (randomIndex === index) {
-          const diffrentStyle = { ...props.style, backgroundColor: 'red' };
           return (
             <div
               style={diffrentStyle}
@@ -25,7 +39,7 @@ const GameZone = (props: {
         }
         return (
           <div
-            style={props.style}
+            style={newStyle}
             key={index}
             data-id="equivalent"
             onClick={props.onClick}
